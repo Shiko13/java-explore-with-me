bab012d53d4b7cbb46577f346aa7d7a1fbbc205a
Sergey <shikofreid@yandex.ru>
2023-02-17 15:49:17 +0400

    @UtilityClass added

--- stats-server/src/main/java/ru/practicum/explorewithme/converter/EndpointHitConverter.java
+++ stats-server/src/main/java/ru/practicum/explorewithme/converter/EndpointHitConverter.java
@@ -1,9 +1,11 @@
 package ru.practicum.explorewithme.converter;
 
+import lombok.experimental.UtilityClass;
 import ru.practicum.explorewithme.dto.EndpointHitDto;
 import ru.practicum.explorewithme.model.App;
 import ru.practicum.explorewithme.model.EndpointHit;
 
+@UtilityClass
 public class EndpointHitConverter {
 
     public static EndpointHit toEndpointHit(EndpointHitDto endpointHitDto, App app) {
