562373932f1e2bc3959c8bf7fa80849159da6166
Sergey <shikofreid@yandex.ru>
2023-03-07 13:04:28 +0400

    Some fixes after review

--- ewm-service/src/main/java/ru/practicum/service/event/EventServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/service/event/EventServiceImpl.java
@@ -399,8 +399,8 @@ public class EventServiceImpl implements EventService {
                     .collect(Collectors.groupingBy(ParticipationRequest::getId, Collectors.summingInt(p -> 1)));
 
             for (int i = 0; i < requestList.size(); i++) {
-                if (eventsRequests.containsKey(i)) {
-                    if ((filteredEvents.get(i).getParticipantLimit() - eventsRequests.get(i)) > 0) {
+                if (eventsRequests.containsKey((long) i)) {
+                    if ((filteredEvents.get(i).getParticipantLimit() - eventsRequests.get((long) i)) > 0) {
                         resultEvents.add(events.get(i));
                     }
                 } else {
